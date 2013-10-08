f2fe4dfcd4d8902e15687ea5b5135294f42da0e6
Frank Zhao <frank.zhao.main@gmail.com>
2013-10-08 17:03:18 -0400

    comment additions

--- optiboot.h
+++ optiboot.h
@@ -1,3 +1,43 @@
+/* VUSBtinyBoot by me@frank-zhao.com
+ *  
+ * VUSBtinyBoot is a bootloader that emulates a USBtinyISP (from Adafruit Industries)
+ *  
+ * Trinket Pro (from Adafruit Industries) will use VUSBtinyBoot
+ *
+ * This code is heavily derived from USBaspLoader, but also from USBtiny, with USBtinyISP's settings
+
+ * This particular file is mostly a copy of optiboot (April 11, 2013 version) http://optiboot.googlecode.com
+ * so that Trinket Pro supports both USB and UART bootloading
+
+   Copyright (c) 2013 Adafruit Industries
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+
+   * Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+   * Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+   * Neither the name of the authors nor the names of its contributors
+     may be used to endorse or promote products derived from this software
+     without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+    ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+    LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #ifndef OPTIBOOT_H_
 #define OPTIBOOT_H_
 
